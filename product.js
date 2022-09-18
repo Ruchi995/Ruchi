9895d2bde7cc6dd0dcfb2b7854a1506c9ec515ef
Ruchi995 <113726776+Ruchi995@users.noreply.github.com>
2022-09-19 02:16:39 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,3 +1,13 @@
+let cartItem = [];
+let cartCount;
+if(localStorage.getItem("cart-count") === null)
+    localStorage.setItem("cart-count","0");
+if(localStorage.getItem("cart-item") === null)
+    localStorage.setItem("cart-item",JSON.stringify(cartItem));
+cartCount = Number(localStorage.getItem("cart-count"));
+let setCartCount = document.getElementById("cart-count");
+setCartCount.innerHTML = cartCount;
+
 let id = Number(new URLSearchParams(window.location.search).get("product_id"));
 if (cartCount === undefined) {
   cartCount = 0;
